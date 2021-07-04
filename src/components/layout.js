c65e569aa86ac770acfd8d109d3342f878fb5326
Xcallibur232 <elliotmichlin@gmail.com>
2021-07-03 20:59:38 -0500

    Added non-index.html redirection

--- src/components/layout.js
+++ src/components/layout.js
@@ -3,9 +3,30 @@ import React from 'react';
 import Header from './header'
 import Footer from './footer'
 import { withRouter } from 'react-router-dom';
+import fetch from 'node-fetch'
 
 class Layout extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = { fetched: false }
+  }
+
   render() {
+
+    /**
+     * Ping API Server
+     */
+    if (this.state.fetched === false) {
+      this.setState({ fetched: true });
+      fetch(`https://api.xcal.dev/core/v1/ping?time=${new Date().getTime()}`, {
+        mode: 'cors',
+        credentials: "include"
+      })
+        .then(res => res.json())
+        .then(json => { });
+    }
+
     const pathname = this.props.location.pathname;
     return (
       <>
